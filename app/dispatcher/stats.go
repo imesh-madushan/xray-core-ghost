da388117eed9d5db957e4900083ae0bb168657e7
imesh-madushan <imeshmadushan927@gmail.com>
2026-04-15 00:05:28 +0530

    re do the updates of domain and size logs

--- app/dispatcher/stats.go
+++ app/dispatcher/stats.go
@@ -1,8 +1,6 @@
 package dispatcher
 
 import (
-	"time"
-
 	"github.com/xtls/xray-core/common"
 	"github.com/xtls/xray-core/common/buf"
 	"github.com/xtls/xray-core/features/stats"
@@ -14,9 +12,7 @@ type SizeStatWriter struct {
 }
 
 func (w *SizeStatWriter) WriteMultiBuffer(mb buf.MultiBuffer) error {
-	if w.Counter != nil && mb != nil {
-		w.Counter.Add(int64(mb.Len()))
-	}
+	w.Counter.Add(int64(mb.Len()))
 	return w.Writer.WriteMultiBuffer(mb)
 }
 
@@ -26,32 +22,4 @@ func (w *SizeStatWriter) Close() error {
 
 func (w *SizeStatWriter) Interrupt() {
 	common.Interrupt(w.Writer)
-}
-
-type SizeStatReader struct {
-	Counter stats.Counter
-	Reader  buf.Reader
-}
-
-func (r *SizeStatReader) ReadMultiBuffer() (buf.MultiBuffer, error) {
-	mb, err := r.Reader.ReadMultiBuffer()
-	if r.Counter != nil && mb != nil {
-		r.Counter.Add(int64(mb.Len()))
-	}
-	return mb, err
-}
-
-func (r *SizeStatReader) ReadMultiBufferTimeout(timeout time.Duration) (buf.MultiBuffer, error) {
-	if tr, ok := r.Reader.(buf.TimeoutReader); ok {
-		mb, err := tr.ReadMultiBufferTimeout(timeout)
-		if r.Counter != nil && mb != nil {
-			r.Counter.Add(int64(mb.Len()))
-		}
-		return mb, err
-	}
-	return r.ReadMultiBuffer()
-}
-
-func (r *SizeStatReader) Interrupt() {
-	common.Interrupt(r.Reader)
 }
